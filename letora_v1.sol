2472dd8a007b34ee05a9b4e07d894ed047b10557
linkeye <40261774+linkeye@users.noreply.github.com>
2020-09-28 16:50:45 +0800

    Update letora_v1.sol
    add fun settle

--- letora_v1.sol
+++ letora_v1.sol
@@ -100,6 +100,41 @@ contract LetContract {
     }
     
     /**
+     * @dev Settle user asset,reback eth and lox token.
+     */  
+    function settle() isOwner public payable{
+        uint arrayLength = buyerList.length;
+        require(arrayLength > 0, "no people to be settle");
+        uint256 feeEth = 0;
+        for (uint i=0; i< arrayLength; i++) {
+            Node memory node = buyerList[i];
+            uint256 ethValue = node.value;
+            uint256 preEth = ethValue.div(100);
+            uint256 rebackEth = preEth.mul(95);
+            uint256 loxTokenValue = ethValue.mul(loxRate);
+
+            uint256 dif = ethValue.sub(rebackEth);
+            feeEth = feeEth.add(dif);
+            
+             // transfer eth
+            node.buyer.transfer(rebackEth);
+            
+            require(
+            loxToken.allowance(msg.sender, address(this)) >= loxTokenValue,
+            "loxToken allowance too low"
+            );
+            // transfer lox
+            _safeTransferFrom(loxToken, msg.sender, node.buyer, loxTokenValue);
+            emit EthSettle(node.buyer,rebackEth,node.blockNumber);
+            emit TokenSettle(node.buyer,loxTokenValue);
+        }
+        // transfer redundant eth to owner.
+        msg.sender.transfer(feeEth);
+        // reset array
+        delete buyerList;
+    }
+    
+    /**
     * @dev Stops / Unstops the contract.
     */
     function toggleContractStopped() isOwner public {
