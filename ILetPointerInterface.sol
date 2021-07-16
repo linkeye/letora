a8a9c11b5cee0ac42cd6e20613fe5358dd4ed55e
linkeye <40261774+linkeye@users.noreply.github.com>
2021-07-16 20:10:34 +0800

    Create ILetPointerInterface.sol
    add pointer interface,return address

---
+++ ILetPointerInterface.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ILetPointerInterface {
+  
+  function getAddress()
+    external
+    view
+    returns (
+      address
+    );
+}
