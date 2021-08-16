221de4e0889683c5e81f33bc2fa1907e90353e86
linkeye <40261774+linkeye@users.noreply.github.com>
2021-08-16 21:04:26 +0800

    Create ILetAggregatorV2V3Interface.sol

---
+++ ILetAggregatorV2V3Interface.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ILetAggregatorInterface.sol";
+import "./ILetAggregatorV3Interface.sol";
+
+interface ILetAggregatorV2V3Interface is ILetAggregatorInterface, ILetAggregatorV3Interface
+{
+}
