e6d1608be8efedb860c5a044f15b41efaf48a878
linkeye <40261774+linkeye@users.noreply.github.com>
2021-09-25 11:17:05 +0800

    delete unuse interface

--- interface/ILetAggregatorV2V3Interface.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "./ILetAggregatorInterface.sol";
-import "./ILetAggregatorV3Interface.sol";
-
-interface ILetAggregatorV2V3Interface is ILetAggregatorInterface, ILetAggregatorV3Interface
-{
-}
