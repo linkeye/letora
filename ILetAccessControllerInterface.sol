678c9174dbf69be2bbd7e941f3e9e0a6d409899b
linkeye <40261774+linkeye@users.noreply.github.com>
2021-09-30 20:42:48 +0800

    Delete ILetAccessControllerInterface.sol

--- ILetAccessControllerInterface.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ILetAccessControllerInterface {
-  function hasAccess(address user, bytes calldata data) external view returns (bool);
-}
